fef10c299264448383e2f3020522c04bd374d03c
Mukul Vishala <119170857+mukullvishala@users.noreply.github.com>
2023-01-06 10:42:50 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,11 @@
+let a = prompt("Hey What is your age?");
+    a = parseInt(a);
+if (a<18) {
+  alert("You are a teenager");
+} else if (a>22) {
+  alert("HERO");
+}
+else {  
+alert("Yes you can drive")
+}
+console.log("DRIVE",(a<18? a<22? "CYCLE YOU KID" :"BIKE CAR" :"ANYTHING"));
